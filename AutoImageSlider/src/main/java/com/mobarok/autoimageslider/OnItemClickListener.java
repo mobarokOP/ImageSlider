328b059380e582c459e12adcb1bedfd65c48acb8
meghnastudio <mobarokmukut.personal@gmail.com>
2025-01-16 14:15:27 +0600

    Initial commit

---
+++ AutoImageSlider/src/main/java/com/mobarok/autoimageslider/OnItemClickListener.java
@@ -0,0 +1,5 @@
+package com.mobarok.autoimageslider;
+
+public interface OnItemClickListener {
+    void onItemClick(int position);
+}
---
+++ app/src/main/java/com/mobarok/imageslider/MainActivity.java
@@ -0,0 +1,67 @@
+package com.mobarok.imageslider;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.mobarok.autoimageslider.ImageSlider;
+import com.mobarok.autoimageslider.OnItemClickListener;
+import com.mobarok.autoimageslider.SlideModel;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        ImageSlider imageSlider = findViewById(R.id.imageSlider);
+        // Replace with your ImageSlider's ID
+
+        ArrayList<SlideModel> imageList = new ArrayList<>();
+        // ↑↑↑ Create a list of SlideModel objects
+
+        imageList.add(new SlideModel(R.drawable.image_1));
+        // ↑↑↑ Only Load Image Form Drawable
+
+        imageList.add(new SlideModel("https://cdn.pixabay.com/photo/2024/05/27/07/46/bee-8790316_640.jpg"));
+        // ↑↑↑ Only Load Image From URL
+
+        imageList.add(new SlideModel("https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_960_720.png","https://github.com/mobarokOP"));
+        // ↑↑↑ Load Image From URL with Open Link
+
+        imageList.add(new SlideModel(R.drawable.image_2, "Title 2", "Description 2"));
+        // ↑↑↑ Load Image From Drawable with Title and Description
+
+        imageList.add(new SlideModel("https://i.ibb.co.com/qgDTsSk/465781910-518730594331898-9051687400886787353-n.jpg", "Mobarok Hossain", "Click Hare to Visit my Facebook", "https://www.facebook.com/mukutOP"));
+        // ↑↑↑ Load Image From URL with Title, Description and Open Link
+
+        imageSlider.setItems(MainActivity.this, imageList);
+        // ↑↑↑ Set the list of SlideModel objects to the ImageSlider
+
+
+        imageSlider.setItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                Toast.makeText(MainActivity.this, "Clicked: "+position, Toast.LENGTH_SHORT).show();
+            }
+        });
+        // ↑↑↑ Set an OnItemClickListener to handle item clicks
+
+
+    }
+}
